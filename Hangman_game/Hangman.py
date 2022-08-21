450303b02e83f8ccebdf36435a2d65a9106a6815
Chu Mai <chuthimai18245@gmail.com>
2022-08-21 17:10:40 +0700

    print more award

--- Hangman_game/Hangman.py
+++ Hangman_game/Hangman.py
@@ -1,99 +1,11 @@
 #Hangman game
 from random import *
-
-situation=['''
-     ____________
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / ]
-     |
- jgs_|___
-''',
-'''
-     ____________
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / 
-     |
- jgs_|___
-'''
-,
-'''
-     ____________
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |    
-     |
- jgs_|___
-'''
-,
-'''
-     ____________
-     |/      |
-     |      (_)
-     |      \|
-     |       |
-     |     
-     |
- jgs_|___
-'''
-,
-'''
-     ____________
-     |/      |
-     |      (_)
-     |       |
-     |       |
-     |      
-     |
- jgs_|___
-'''
-,
-'''
-     ____________
-     |/      |
-     |      (_)
-     |      
-     |       
-     |      
-     |
- jgs_|___
-'''
-,
-'''
-     ____________
-     |/      |
-     |     
-     |      
-     |       
-     |      
-     |
- jgs_|___
-'''
-           ]
+from Hangman_art import *
 
 from Hangman_word import word_list
 word=choice(word_list)
 
-print('''
-888                                                           
-888                                                           
-888                                                           
-88888b.     8888b.  88888b.    .d88bbb  88888b.d88b.     8888b.  88888b.  
-888 "88b      "88b  888 "88b  d88P"88b  888 "888 "88b      "88b  888 "88b 
-888  888  .d888888  888  888  888  888  888  888  888  .d888888  888  888 
-888  888  888  888  888  888  Y88b 888  888  888  888  888  888  888  888 
-888  888  "Y888888  888  888   "Y88888  888  888  888  "Y888888  888  888 
-                                   888                              
-                              Y8b d88P                              
-                               "Y88P"                               
-''')   #logo
+print(logo)   #logo
 
 # print(f"the word was chosen is {word}")
 
@@ -131,6 +43,6 @@ while count_!=0 and lives!=0:
     print(word_display)
 
     if count_ == 0:
-        print(f"YOU WIN!\nThe word correct ís \"{word}\"")
+        print(f"YOU WIN!\n{prise}\nThe word correct ís \"{word}\"")
     if lives==0:
         print("YOU LOSE! :(")
