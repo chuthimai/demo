c03b4cadbc9d9407661254008e5587895a63bb54
Chu Mai <chuthimai18245@gmail.com>
2022-10-27 20:17:43 +0700

    Change draw.png Change draw sawtooth

--- draw.py
+++ draw.py
@@ -1,23 +1,27 @@
 from matplotlib.pyplot import *
 from numpy import *
-from math import *
 from cmath import *
+from scipy import signal # xu ly tin hieu co sawtooth va square
 
-n = linspace(0, 40, 40)
-s = complex(-1/12, pi/6)
-K = 2
-x = []
-for i in n:
-    x.append(K*exp(s*i))
+A = 3
+L = 100
+N = 15
+Fs = 20
+DRX = 60
+Ts = 1/Fs
+t = linspace(0, L, L)
 
-subplot(2, 1, 1)
-stem(n, real(x))
-title("Phan thuc")
-axis([0, 40, -2, 2])
+subplot(2,1,1)
+x1 = A*signal.sawtooth(2*pi*t/N)
+stem(t, x1)
+xlabel("Thoi gian (s)")
+ylabel("Bien do")
+title("Day xung rang cua")
 
-subplot(2, 1, 2)
-stem(n, imag(x))
-title("Phan ao")
-axis([0, 40, -2, 2])
-# show()
+subplot(2,1,2)
+x2 = A*signal.square(2*pi*t/N, DRX/L)
+stem(t, x2)
+xlabel("Thoi gian (s)")
+ylabel("Bien do")
+title("Day xung vuong")
 savefig("draw.png")
