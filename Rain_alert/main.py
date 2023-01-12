a1752629e77ab5037bd117d238122d127def0f86
Chu Mai <chuthimai18245@gmail.com>
2023-01-12 20:28:07 +0700

    Rain forecast

---
+++ Rain_alert/main.py
@@ -0,0 +1,45 @@
+import requests
+from twilio.rest import Client
+
+account_sid = "ID"
+auth_token = "AUTH_TOKEN"
+api_key = "API_KEY"
+# location: Hai Duong, Vietnam
+parameters = {
+    "lat": 20.937342,
+    "lon": 106.314552,
+    "appid": api_key,
+}
+
+response = requests.get(
+    url="https://api.openweathermap.org/data/2.5/forecast",
+    params=parameters,
+)
+
+data = response.json()["list"]
+weather_data = []
+for i in range(4):
+    weather_data.append(data[i]['weather'][0]['id'])
+print(weather_data)
+
+will_rain = False
+for i in range(4):
+    if weather_data[i] < 700:
+        will_rain = True
+        break
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an umbrella ☔️☔☔",
+        from_="+12528887392",
+        to="+84987169250"
+    )
+    print(message.status)
+
+
+
+
+
+
+
+
