0defffd0fb13a2cb5b1648931321bb34be9b9eb9
Chu Mai <chuthimai18245@gmail.com>
2023-09-05 22:15:11 +0700

    update selenium

--- Web/Collect Data/main.py
+++ Web/Collect Data/main.py
@@ -8,7 +8,7 @@ GG_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSfdu3rDQ18JQWqUCToZMYTR9AR_z
 ZILLOW_LINK = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22mapBounds%22%3A%7B%22west%22%3A-128.25608290625001%2C%22east%22%3A-116.61057509375001%2C%22south%22%3A32.81061121465328%2C%22north%22%3A42.427730472853206%7D%2C%22mapZoom%22%3A6%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
 
 header = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 }
 
 
@@ -37,7 +37,7 @@ def write_to_csv(add, p, l):
 
 
 def get_data():
-    time.sleep(15)
+    time.sleep(20)
     find_grid = driver.find_elements(By.TAG_NAME, "article")
 
     price = []
--- Web/Internet Speed Twitter Complaint Bot/main.py
+++ Web/Internet Speed Twitter Complaint Bot/main.py
@@ -19,7 +19,7 @@ class InternetSpeedTwitterBot():
         self.driver.get("https://www.speedtest.net/")
         go_button = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
         go_button.click()
-        time.sleep(40)
+        time.sleep(60)
         self.down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
         self.up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
         print(self.down)
